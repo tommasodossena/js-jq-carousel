8e48388171fd8e580c765619b5b90f58262afcca
Tommaso <dossena.tommaso.dt@gmail.com>
2021-03-12 16:11:41 +0100

    Arrow click script

--- js/main.js
+++ js/main.js
@@ -11,5 +11,35 @@ Generiamo i pallini con JS
 
 // 0 - $(document).ready()
 $(function() {
-    console.log( "ready!" );
+    
+    // 1A - Al click sulla freccia sinistra, mostrare l'immagine precedente
+    $(".prev").click(
+        function() {
+            // 2A - Variabile che seleziona l'immagine con la classe active e la rimuove
+            var imgActive = $(".active");
+            imgActive.removeClass("active");
+            // 3A - Se l'immagine con la classe active ha la classe first, devo applicare la classe active all'ultima immagine. 
+            if ( imgActive.hasClass("first") == false ) {
+                imgActive.prev().addClass("active");   
+            } else {
+                $(".last").addClass("active");  
+            }
+        }
+    );
+
+    // 1B - Al click sulla freccia destra, mostrare l'immagine successiva
+    $(".next").click(
+        function() {
+            // 2B - Variabile che seleziona l'immagine con la classe active e la rimuove
+            var imgActive = $(".active");
+            imgActive.removeClass("active");
+            // 3B - Se l'immagine con la classe active ha la classe last, devo applicare la classe active alla prima immagine. 
+            if ( imgActive.hasClass("last") == false ) {
+                imgActive.next().addClass("active");   
+            } else {
+                $(".first").addClass("active");  
+            }
+        }
+    );
+
 });
