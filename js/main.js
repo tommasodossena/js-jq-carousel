f5ad91f410eada69b1072f59b870bf211842413b
Tommaso <dossena.tommaso.dt@gmail.com>
2021-03-12 17:52:14 +0100

    Keydown script

--- js/main.js
+++ js/main.js
@@ -9,35 +9,58 @@ Clicchiamo sui pallini e mostriamo l’immagine corrispondente
 Generiamo i pallini con JS
 */
 
+// FUNZIONI
+// Funzione immagine precedente carosello
+function prevImage() {
+    // 1A - Variabile che seleziona l'immagine con la classe active e la rimuove
+    var imgActive = $(".active");
+    imgActive.removeClass("active");
+    // 2A - Se l'immagine con la classe active ha la classe first, devo applicare la classe active all'ultima immagine. 
+    if ( imgActive.hasClass("first") == false ) {
+        imgActive.prev().addClass("active");   
+    } else {
+        $(".last").addClass("active");  
+    }
+}
+
+// Funzione immagine successiva carosello
+function nextImage() {
+    // 1B - Variabile che seleziona l'immagine con la classe active e la rimuove
+    var imgActive = $(".active");
+    imgActive.removeClass("active");
+    // 2B - Se l'immagine con la classe active ha la classe last, devo applicare la classe active alla prima immagine. 
+    if ( imgActive.hasClass("last") == false ) {
+        imgActive.next().addClass("active");   
+    } else {
+        $(".first").addClass("active");  
+    }
+}
+
+
 // 0 - $(document).ready()
 $(function() {
     
-    // 1A - Al click sulla freccia sinistra, mostrare l'immagine precedente
+    // 1 - Al click sulla freccia sinistra, mostrare l'immagine precedente
     $(".prev").click(
         function() {
-            // 2A - Variabile che seleziona l'immagine con la classe active e la rimuove
-            var imgActive = $(".active");
-            imgActive.removeClass("active");
-            // 3A - Se l'immagine con la classe active ha la classe first, devo applicare la classe active all'ultima immagine. 
-            if ( imgActive.hasClass("first") == false ) {
-                imgActive.prev().addClass("active");   
-            } else {
-                $(".last").addClass("active");  
-            }
+            prevImage();
         }
     );
 
-    // 1B - Al click sulla freccia destra, mostrare l'immagine successiva
+    // 2 - Al click sulla freccia destra, mostrare l'immagine successiva
     $(".next").click(
         function() {
-            // 2B - Variabile che seleziona l'immagine con la classe active e la rimuove
-            var imgActive = $(".active");
-            imgActive.removeClass("active");
-            // 3B - Se l'immagine con la classe active ha la classe last, devo applicare la classe active alla prima immagine. 
-            if ( imgActive.hasClass("last") == false ) {
-                imgActive.next().addClass("active");   
-            } else {
-                $(".first").addClass("active");  
+            nextImage();
+        }
+    );
+   
+    // 3 - Premendo le frecce direzionali sulla tastiera l'immagine cambia
+    $(document).keydown(
+        function (e) {
+            if ( e.keyCode == 39 ) {
+                nextImage();
+            } else if ( e.keyCode == 37 ) { 
+                prevImage();
             }
         }
     );
